09887aeab97381d1a31da324b28ebad6a01b07c1
kartul <derek.maekask@gmail.com>
2017-08-25 03:02:23 +0300

    It's alive! found bug tho :(

--- geofencer.py
+++ geofencer.py
@@ -2,15 +2,23 @@
 import ujson
 import urllib2
 import csv
+import configargparse
 def main():
 
-    raw = urllib2.urlopen("https://nominatim.openstreetmap.org/search.php?q='tartu'}&polygon_geojson=1&format=json").read()
+    parser = configargparse.ArgParser(description='geoFenceGen')
+    parser.add_argument('-dir', '--directory', help='Directory and filename of geofence. ex: geofence/bratislava.txt', required=True)
+    parser.add_argument('-l', '--location', help='City/Country/areas you wouls like the geofence for', required=True)
+    args = parser.parse_args()
+
+    end = "https://nominatim.openstreetmap.org/search.php?q='{}'&polygon_geojson=1&format=json".format(args.location)
+    raw = urllib2.urlopen(end).read()
     geojson = ujson.loads(raw)[0]['geojson']
     coords = geojson['coordinates']
     loc = (coords)[0]
-    with open('output.txt', 'w') as f:
+    l = [["{}".format(args.location)]]
+    with open('{}'.format(args.directory), 'wb') as f:
         writer = csv.writer(f)
-        writer.writerow
+        writer.writerow(l)
         writer.writerows(loc)
 
 
