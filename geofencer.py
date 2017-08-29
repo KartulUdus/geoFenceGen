35f240375fc20e5c557b6103eb246dd827cb1faf
Derek Mäekask <derek.maekask@myjar.com>
2017-08-29 23:07:52 +0300

    MUHAHAHAHAHAHA

--- geofencer.py
+++ geofencer.py
@@ -12,67 +12,75 @@ def main():
     parser.add_argument(
         '-dir',
         '--directory',
-        help='Directory and filename of geofence. ex: geofence/bratislava.txt',
+        help='Directory and filename of geofence. ex: geofence/Bratislava.txt',
         required=True)
     parser.add_argument(
         '-l',
         '--location',
-        help='City/Country/areas you wouls like the geofence for',
+        help='City/Country/area you would like a geofence for',
         required=True)
+
     parser.add_argument(
         '-w',
         '--way',
-        help=('Use if there\'s no relational polygon'),
+        help=('Use if there\'s no relational polygon' +
+              '(ex: Manhattan central park'),
         action='store_true',
         default=False)
     parser.add_argument(
         '-mp',
-        '--multi-polygon',
+        '--multiPolygon',
         help=('Use if you wish to write all polygons of an area'),
         action='store_true',
         default=False)
-
     args = parser.parse_args()
-    end = "https://nominatim.openstreetmap.org/search.php?q='{}'&polygon_geojson=1&format=json".format(
-        args.location)
+    loc = (args.location)
+    loc = loc.replace(" ", "+")
+    end = ("https://nominatim.openstreetmap.org/"
+           "search.php?q='{}'&polygon_geojson=1&format=json".format(loc))
 
-# Get polygon
-    raw = json.load(urllib2.urlopen(end))
-    coords = [x for x in raw if x['osm_type'] ==
-              'relation'][0]['geojson']['coordinates']
+# Check for good response and get polygon
 
-# check if polygon is multipolygon
+    if (len(json.load(urllib2.urlopen(end))) < 1):
+        print 'Unable to find areas, exiting'
+        exit(1)
+    else:
+        raw = json.load(urllib2.urlopen(end))
+        mode = 'way' if (args.way) else 'relation'
+        coords = [x for x in raw if x['osm_type'] ==
+                  '{}'.format(mode)][0]['geojson']['coordinates']
 
-    print(len(coords[0][0]))
+# check if area has single polygon and write
 
-    if (len(coords[0][0]) == 2):
+    if (len(coords[0][0]) == 2 or (args.way)):
         # Write fenceFile
         l = [["{}".format(args.location)]]
         with open('{}'.format(args.directory), 'wb') as f:
-
             writer = csv.writer(f)
             writer.writerow(l)
             for row in (coords[0]):
                 writer.writerow(row[::-1])
 
         print(
-            "your geofence of {} has been created in: {}".format(
+            "Geofence of {} has been created in: {}".format(
                 args.location,
                 args.directory))
 
     else:
-        # Write fenceFile
+        # Write fenceFile or multiFence
+
         with open('{}'.format(args.directory), 'wb') as f:
             writer = csv.writer(f)
-            for z in (coords):
-                n = 0
-                l = [["{}{}".format(args.location, n + 1)]]
+            mp = coords if (args.multiPolygon) else coords[0]
+            n = 0
+            for row in (mp):
+                l = [["{} {}".format(args.location, n + 1)]]
                 writer.writerow(l)
-                for row in (coords[0][n]):
+                for row in (coords[n][0]):
                     writer.writerow(row[::-1])
                 n += 1
             print(
-                "Multiplygon geofence of {} has been created in: {}".format(
+                "Geofence of {} has been created in: {}".format(
                     args.location, args.directory))
 
 
