ac68a556d7103a4fc4748be937080c36afbf3dca
Derek Mäekask <derek.maekask@myjar.com>
2017-08-29 11:51:43 +0300

    merged test.py to geofencer.py

--- geofencer.py
+++ geofencer.py
@@ -5,27 +5,78 @@ import urllib2
 import csv
 import configargparse
 
+
 def main():
-##ARGS
+
+    # ARGS
     parser = configargparse.ArgParser(description='geoFenceGen')
-    parser.add_argument('-dir', '--directory', help='Directory and filename of geofence. ex: geofence/bratislava.txt', required=True)
-    parser.add_argument('-l', '--location', help='City/Country/areas you wouls like the geofence for', required=True)
+    parser.add_argument(
+        '-dir',
+        '--directory',
+        help='Directory and filename of geofence. ex: geofence/bratislava.txt',
+        required=True)
+    parser.add_argument(
+        '-l',
+        '--location',
+        help='City/Country/areas you wouls like the geofence for',
+        required=True)
+    parser.add_argument(
+        '-w',
+        '--way',
+        help=('Use if the area does not have a relational polygon, but has a type:way'),
+        action='store_true',
+        default=False)
+    parser.add_argument(
+        '-mp',
+        '--multi-polygon',
+        help=('Use if you wish to write all polygons of an area'),
+        action='store_true',
+        default=False)
+
     args = parser.parse_args()
     wd = os.getcwd()
-    end = "https://nominatim.openstreetmap.org/search.php?q='{}'&polygon_geojson=1&format=json".format(args.location)
+    end = "https://nominatim.openstreetmap.org/search.php?q='{}'&polygon_geojson=1&format=json".format(
+        args.location)
 
-## Get polygon
+# Get polygon
     raw = json.load(urllib2.urlopen(end))
-    coords = [x for x in raw if x['osm_type'] == 'relation'][0]['geojson']['coordinates'][0]
+    coords = [x for x in raw if x['osm_type'] ==
+              'relation'][0]['geojson']['coordinates']
 
-##Write fenceFile
-    l = [["{}".format(args.location)]]
-    with open('{}'.format(args.directory), 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerow(l)
-        for row in (coords):
-               writer.writerow(row[::-1])
+# check if polygon is multipolygon
 
+    print(len(coords[0][0]))
+
+    if (len(coords[0][0]) == 2):
+        # Write fenceFile
+        l = [["{}".format(args.location)]]
+        with open('{}'.format(args.directory), 'wb') as f:
+
+            writer = csv.writer(f)
+            writer.writerow(l)
+            for row in (coords[0]):
+                writer.writerow(row[::-1])
+
+        print(
+            "your geofence of {} has been created in: \n{}\{}".format(
+                args.location,
+                wd,
+                args.directory))
+
+    else:
+        # Write fenceFile
+        with open('{}'.format(args.directory), 'wb') as f:
+            writer = csv.writer(f)
+            for z in (coords):
+                n = 0
+                l = [["{}{}".format(args.location, n + 1)]]
+                writer.writerow(l)
+                for row in (coords[0][n]):
+                    writer.writerow(row[::-1])
+                n += 1
+            print(
+                "your multiplygon geofence of {} has been created in: \n{}\{}".format(
+                    args.location, wd, args.directory))
 
 
 if __name__ == "__main__":
